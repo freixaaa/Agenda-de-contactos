de14c7c9859ef6b8389200f222dfa9117786f0ae
freixaaa <fiorella07freixa23.r@gmail.com>
2026-03-26 18:41:32 -0600

    Update contacto.py

--- modelos/contacto.py
+++ modelos/contacto.py
@@ -1,16 +1,20 @@
 class contacto:
-    def __init__(self, nombre, telefono, email, edad, residencia):
-        self.nombre = nombre
-        self.telefono = telefono
-        self.email = email
-        self.edad = int(edad)
-        self.residencia = residencia
+    def __init__(self, **kwargs):
+        self.nombre = kwargs.get("nombre", "")
+        self.telefono = kwargs.get("telefono", "")
+        self.email = kwargs.get("email", "")
+        self.residencia = kwargs.get("residencia", "")
+        self.salario = kwargs.get("salario", "")
+        self.peso = kwargs.get("peso", "")
+        self.edad = kwargs.get("edad", "")
 
     def to_dict(self):
         return {
             "nombre": self.nombre,
             "telefono": self.telefono,
             "email": self.email,
-            "edad": self.edad,
-            "residencia": self.residencia
+            "residencia": self.residencia,
+            "salario": self.salario,
+            "peso": self.peso,
+            "edad": self.edad
         }
